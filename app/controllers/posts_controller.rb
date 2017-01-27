08eddf945692bb8a2942ed28007125ea07d317d0
Olena Dykhno <olenadykhno@gmail.com>
2017-01-27 17:46:50 +0200

    add plugin for calendar, implement posting to FB page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,27 @@ class PostsController < ApplicationController
   end
 
   def create
+    if Time.new(*time_parsing(post_params[:time])).to_i > (Time.now + 600).to_i
+      FacebookAPI.publish_post_by_schedule(Rails.application.secrets.page_access_token, post_params[:text], Time.new(*time_parsing(post_params[:time])).to_i)
+      flash[:message] = 'Your post will be published at the scheduled time'
+    else
+      FacebookAPI.publish_post_immediately(Rails.application.secrets.page_access_token, post_params[:text])
+      flash[:message] = 'Your post is published'
+    end
+    redirect_to new_post_path
   end
 
   private
   def check_page_token_existence
     redirect_to new_token_path if Rails.application.secrets.page_access_token.nil?
+  end
+
+  def post_params
+    params.require(:post).permit(:text, :time)
+  end
+
+  def time_parsing(string)
+    string.gsub('/', ' ').gsub(':', ' ').split.map { |el| el.to_i }
   end
 
 end
--- lib/facebook_api.rb
+++ lib/facebook_api.rb
@@ -1,9 +1,19 @@
 class FacebookAPI
   class << self
-
-    def get_token_of_page(user_token)
-      Koala::Facebook::API.new(user_token).get_connections("me", "accounts").first['access_token']
+    def create_new_graph_api(page_token)
+      Koala::Facebook::API.new(page_token)
     end
 
+    def get_token_of_page(user_token)
+      create_new_graph_api(user_token).get_connections("me", "accounts").first['access_token']
+    end
+
+    def publish_post_by_schedule(page_token, text, time)
+      create_new_graph_api(page_token).put_wall_post(text, {published: false, scheduled_publish_time: time})
+    end
+
+    def publish_post_immediately(page_token, text)
+      create_new_graph_api(page_token).put_wall_post(text)
+    end
   end
 end
